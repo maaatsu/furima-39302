fa1b84f90548453a95b95278e07ca65ee90f4cce
maaatsu <bigbear.honey.87@gmail.com>
2023-06-17 15:28:33 +0900

    user.rb　バリデーション入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :date_of_birth, presence: true
+
 end
