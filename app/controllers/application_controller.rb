adc10e28364b573e9c2e80cb11c7f6796507767c
maaatsu <bigbear.honey.87@gmail.com>
2023-06-21 12:23:34 +0900

    usersテーブルカラム追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:hoge, :hoge_1i, :hoge_2i, :hoge_3i])  # 許可するパラメーターを追加してください
     end
   end
-end
