1ef3e532e85ced54dd6ffe2a63500d4611a968ba
maaatsu <bigbear.honey.87@gmail.com>
2023-06-22 18:14:53 +0900

    新規登録バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: "は半角英数字混合で入力してください" }
+  validates :password_confirmation, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
