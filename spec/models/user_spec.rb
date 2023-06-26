7086ec9c7d2d9b903bdeeb85dbeb6b872937bc5c
maaatsu <bigbear.honey.87@gmail.com>
2023-06-26 18:24:01 +0900

    it3つへ分割"名前カナ入力テストコード"

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -140,17 +140,21 @@ RSpec.describe User, type: :model do
         expect(@user.errors[:last_name_kana]).to be_empty
       end
 
-      it 'お名前カナ(全角)は、全角（カタカナ）での入力が必須であること' do
+      it 'お名前カナ名字(全角)は、全角（カタカナ）での入力が必須であること' do
         @user.first_name_kana = 'Yamada'
         @user.last_name_kana = 'タロウ'
         @user.valid?
         expect(@user.errors[:first_name_kana]).to include('は全角カタカナで入力してください')
-      
+      end
+
+      it 'お名前カナ名前(全角)は、全角（カタカナ）での入力が必須であること' do      
         @user.first_name_kana = 'タロウ'
         @user.last_name_kana = 'Yamada'
         @user.valid?
         expect(@user.errors[:last_name_kana]).to include('は全角カタカナで入力してください')
-      
+      end
+
+      it 'お名前カナ(全角)は、全角（カタカナ）での入力が必須であること' do      
         @user.first_name_kana = 'タロウ'
         @user.last_name_kana = 'ヤマダ'
         @user.valid?
