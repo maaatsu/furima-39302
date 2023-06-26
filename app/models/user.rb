78e5fa28434e0277762e516ee0cd2bff11e0de3c
maaatsu <bigbear.honey.87@gmail.com>
2023-06-26 15:06:07 +0900

    last_name,first_name正規表現変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'は半角英数字混合で入力してください' }
-  validates :last_name, presence: { message: 'お名前(全角)を入力してください' }, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください' }
-  validates :first_name, presence: { message: 'お名前(全角)を入力してください' }, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください' }  
+  validates :last_name, presence: { message: 'お名前(全角)を入力してください' }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください' }
+  validates :first_name, presence: { message: 'お名前(全角)を入力してください' }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください' }  
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナで入力してください' }
   validates :date_of_birth, presence: { message: 'を入力してください' }
