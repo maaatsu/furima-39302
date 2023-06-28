2d3cd95de62a4dfbc88404b70d03ceed72d6095f
maaatsu <bigbear.honey.87@gmail.com>
2023-06-28 16:39:33 +0900

    商品の状態の情報が必須であること

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,6 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :category_id, presence: true
-
+  validates :status, presence: true
+  
 end
