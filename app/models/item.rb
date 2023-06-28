d8cf526ad9f1dacc79b97265ccbb256d07cbe072
maaatsu <bigbear.honey.87@gmail.com>
2023-06-28 17:02:28 +0900

    発送元の地域の情報が必須であること

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,5 +5,6 @@ class Item < ApplicationRecord
   validates :category_id, presence: true
   validates :status, presence: true
   validates :shipping_fee, presence: true
+  validates :region_of_origin_id, presence: true
   
 end
