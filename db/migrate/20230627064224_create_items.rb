553ea61c94ae3acb443cbfc17e8669ec82449007
maaatsu <bigbear.honey.87@gmail.com>
2023-07-01 16:45:57 +0900

    「出品する」ボタンを押すと商品情報がデータベースに保存される

--- db/migrate/20230627064224_create_items.rb
+++ db/migrate/20230627064224_create_items.rb
@@ -1,6 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string     :image,               null: false
+      t.string     :name,                null: false
+      t.text       :description,         null: false
+      t.integer    :category_id,         null: false
+      t.integer    :status,              null: false
+      t.integer    :shipping_fee_status, null: false
+      t.integer    :prefecture,          null: false
+      t.integer    :scheduled_delivery,  null: false
+      t.integer    :price,               null: false
+
     end
   end
 end
---
+++ db/migrate/20230630023350_add_columns_to_items.rb
@@ -0,0 +1,11 @@
+class AddColumnsToItems < ActiveRecord::Migration[6.0]
+  def change
+# add_reference(:items, :user, {:null=>false, :foreign_key=>true})
+# add_column :items, :category_id, :integer, null: false
+# add_column :items, :situation_id, :integer, null: false
+# add_column :items, :shipping_charge_id, :integer, null: false
+# add_column :items, :region_of_origin_id, :integer, null: false
+# add_column :items, :ship_date_id, :integer, null: false
+# add_column :items, :price, :integer, null: false
+  end
+end
