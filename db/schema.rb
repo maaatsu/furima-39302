7d18e80296d553342e69f0db9a19670952913c66
maaatsu <bigbear.honey.87@gmail.com>
2023-07-03 18:56:08 +0900

    itemsテーブルへuser_id再導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_30_103116) do
+ActiveRecord::Schema.define(version: 2023_07_03_071107) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2023_06_30_103116) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
@@ -40,9 +48,10 @@ ActiveRecord::Schema.define(version: 2023_06_30_103116) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "image"
     t.string "name", null: false
     t.text "description", null: false
-    t.bigint "user_id", null: false
     t.integer "category_id", null: false
     t.integer "status", null: false
     t.integer "shipping_fee_status", null: false
@@ -71,4 +80,5 @@ ActiveRecord::Schema.define(version: 2023_06_30_103116) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
