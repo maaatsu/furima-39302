a0b2d192e6b4b9c6dd3348b08ed18c36801ceb20
maaatsu <bigbear.honey.87@gmail.com>
2023-07-09 12:26:38 +0900

    【修正】shipping_fee_statusアクティブハッシュ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,29 +3,27 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   
   def index
-    render 'items/index'
     @items = Item.order("created_at DESC")
+    render 'items/index'
   end
 
   def new
     @item = Item.new
     @categories = Category.all
-    @shipping_fee_statuses = Shipping_fee_status.all
-    @prefecture = Prefecture.all
+    @shipping_fee_statuses = ShippingFeeStatus.all
+    @prefectures = Prefecture.all
   end
 
   def create
     @item = Item.new(item_params)
     @item.user = current_user
-    @shipping_fee_statuses = Shipping_fee_status.all
-    
     
     if @item.save
       redirect_to root_path
     else
       @categories = Category.all
-      @shipping_fee_statuses = Shipping_fee_status.all
-      @prefecture = Prefecture.all
+      @shipping_fee_statuses = ShippingFeeStatus.all
+      @prefectures = Prefecture.all
 
       render :new
     end
