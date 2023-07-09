a876c993e70028c16a6beb203c83093c2af76936
maaatsu <bigbear.honey.87@gmail.com>
2023-07-09 12:45:04 +0900

    【修正】販売手数料・利益js

--- app/javascript/packs/selling_fee.js
+++ app/javascript/packs/selling_fee.js
@@ -3,17 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const addTaxPrice = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
 
-  priceInput.addEventListener("input", () => {
+  const updateFeeAndProfit = () => {
     const price = parseInt(priceInput.value, 10);
-    const tax = Math.floor(price * 0.1);
-    const profitAmount = Math.floor(price - tax);
 
     if (isNaN(price)) {
       addTaxPrice.textContent = "0";
       profit.textContent = "0";
     } else {
+      const tax = Math.floor(price * 0.1);
+      const profitAmount = Math.floor(price - tax);
       addTaxPrice.textContent = tax.toLocaleString();
       profit.textContent = profitAmount.toLocaleString();
     }
-  });
+  };
+
+  priceInput.addEventListener("input", updateFeeAndProfit);
+  updateFeeAndProfit(); // 初期表示時にも計算する
 });
