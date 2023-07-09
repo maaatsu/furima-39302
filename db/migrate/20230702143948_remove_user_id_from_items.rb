f7f21c13a23b1db51adde0140925cbc1f8b13838
maaatsu <bigbear.honey.87@gmail.com>
2023-07-09 22:06:36 +0900

    不要マイグレーションファイル削除

--- db/migrate/20230702143948_remove_user_id_from_items.rb
+++
@@ -1,8 +0,0 @@
-class RemoveUserIdFromItems < ActiveRecord::Migration[6.0]
-  def change
-    def change
-      remove_index :items, name: "index_items_on_user_id"
-      remove_column :items, :user_id
-    end   
-  end
-end
