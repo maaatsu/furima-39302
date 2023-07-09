5621ada9b3df9ee9808011cdaf7724f7f1642792
maaatsu <bigbear.honey.87@gmail.com>
2023-07-09 22:21:14 +0900

    item_specのFactoryBotをuserと紐付け削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   describe '商品出品機能' do
     before do
-      user = FactoryBot.create(:user)
-      @item = FactoryBot.build(:item, user_id: user.id)
+      @item = FactoryBot.build(:item)
     end
 
     context '正常な場合' do
