0c221763b883dedb429f2832a77c6abf4a2b0e9a
maaatsu <bigbear.honey.87@gmail.com>
2023-07-09 23:26:45 +0900

    【追加】itemバリデーション

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,10 +37,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = 1 # カテゴリーに「---」の値
+        @item.valid?
+        expect(@item.errors[:category_id]).to include("can't be blank")
+      end
+
       it '商品の状態の情報が必須であること' do
         @item.status_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.status_id = 1 # 商品の状態に「---」の値
+        @item.valid?
+        expect(@item.errors[:status_id]).to include("can't be blank")
       end
 
       it '配送料の負担の情報が必須であること' do
@@ -49,16 +61,34 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
       end
 
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.shipping_fee_status_id = 1 # 配送料の負担に「---」の値
+        @item.valid?
+        expect(@item.errors[:shipping_fee_status_id]).to include("can't be blank")
+      end
+
       it '発送元の地域の情報が必須であること' do
         @item.prefecture_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
 
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = 1 # 発送元の地域に「---」の値
+        @item.valid?
+        expect(@item.errors[:prefecture_id]).to include("can't be blank")
+      end
+
       it '発送までの日数の情報が必須であること' do
         @item.scheduled_delivery_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
+      end
+
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.scheduled_delivery_id = 1 # 発送までの日数に「---」の値
+        @item.valid?
+        expect(@item.errors[:scheduled_delivery_id]).to include("can't be blank")
       end
 
       it '価格の情報が必須であること' do
@@ -83,6 +113,12 @@ RSpec.describe Item, type: :model do
         @item.price = '１０００' # 全角数字
         @item.valid?
         expect(@item.errors[:price]).to include('Price is invalid. Input half-width characters')
+      end
+      
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors[:user]).to include("must exist")
       end
     end
   end
