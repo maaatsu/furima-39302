d3510f6f76342560efb20a5cd647f92eb59cddd1
maaatsu <bigbear.honey.87@gmail.com>
2023-07-09 12:27:15 +0900

    【修正】priceバリデーション出力

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,12 +14,16 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :scheduled_delivery, presence: { message: "can't be blank" }
-  validates :price, presence: { message: "can't be blank" },
-                    numericality: { only_integer: true,
-                                  greater_than_or_equal_to: 300,
-                                  less_than_or_equal_to: 9_999_999,
-                                  message: "is out of setting range" }
-  validates :price, presence: true,
-                      format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
-                                
+  validates :price, presence: { message: "Price can't be blank" },
+                    numericality: {
+                      only_integer: true,
+                      greater_than_or_equal_to: 300,
+                      less_than_or_equal_to: 9_999_999,
+                      message: "Price is out of setting range"
+                    },
+                    format: {
+                      with: /\A[0-9]+\z/,
+                      message: "Price is invalid. Input half-width characters"
+                    }
+
 end
