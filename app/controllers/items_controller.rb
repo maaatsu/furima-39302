097ca3e58fa14e91b0cb7f9929513425c117ccce
maaatsu <bigbear.honey.87@gmail.com>
2023-07-16 12:12:47 +0900

    ログイン状態の場合でも、自身が出品していない商品の商品情報編集ページへ遷移しようとすると、商品の販売状況に関わらずトップページに遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :check_ownership, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -45,10 +47,28 @@ class ItemsController < ApplicationController
     @scheduled_deliveries = ScheduledDelivery.all
 end
 
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id,
                                  :scheduled_delivery_id, :price)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_ownership
+    unless @item.user == current_user
+      redirect_to root_path
+    end
   end
 end
