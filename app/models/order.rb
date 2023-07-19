493dfa6b9c44efe60880fb1e3d31d74055dee69c
maaatsu <bigbear.honey.87@gmail.com>
2023-07-19 16:04:37 +0900

    order_shipping_addressバリデーション設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,4 @@ class Order < ApplicationRecord
   belongs_to :user
   has_one :shipping_address
 
-  validates :token, presence: { message: "can't be blank" }
-
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -8,23 +8,14 @@ class OrderShippingAddress
   validates :expiry_date, format: { with: /\A\d{2}\/\d{2}\z/ }
   validates :security_code, length: { is: 3 }, numericality: { only_integer: true }
 
-  def save
-    if valid?
-      save_shipping_info
-      save_purchase_info
-      true
-    else
-      false
-    end
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+    validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+    validates :city
+    validates :addresses
+    validates :phone_number, length: { minimum: 10, message: "is too short" }, format: { with: /\A\d+\z/, message: "is invalid. Input only number" }
+  
   end
 
-  private
-
-  def save_shipping_info
-    ShippingInfo.create(address: shipping_address)
-  end
-
-  def save_purchase_info
-    Purchase.create(address: billing_address, credit_card_number: credit_card_number, expiry_date: expiry_date, security_code: security_code)
-  end
 end
