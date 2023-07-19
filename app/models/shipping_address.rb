12acd393b29b993e77e9ed4e7ecc473831072bfb
maaatsu <bigbear.honey.87@gmail.com>
2023-07-19 12:49:16 +0900

    shipping_address　バリデーション設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,10 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :order
+
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :addresses, presence: true
+  validates :phone_number, presence: true, length: { minimum: 10, message: "is too short" }, format: { with: /\A\d+\z/, message: "is invalid. Input only number" }
+
 end
