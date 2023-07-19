58f1f237c4ae12a31ef2b25066c2f5733694e18e
maaatsu <bigbear.honey.87@gmail.com>
2023-07-19 12:30:32 +0900

    createメソッド編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,18 +7,14 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @purchase_form = PurchaseForm.new(purchase_params)
-    if @purchase_form.save
-      # 購入成功時の処理
-      redirect_to root_path
-    else
-      render :index
-    end
+    @order = Order.create(order_params)
+    ShippingAddress.create(shipping_address_params)
+    redirect_to root_path
   end
 
   private
 
-  def purchase_params
-    params.require(:purchase_form).permit(:shipping_address, :billing_address, :credit_card_number, :expiry_date, :security_code).merge(item_id: params[:item_id], user_id: current_user.id)
+  def shipping_address_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building_name, :telephone_number, :order).merge(order_id: @order.id)
   end
 end
